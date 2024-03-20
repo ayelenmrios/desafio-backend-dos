3f446016648493d449122a9d9e1fbc1e6045e204
Ayelen <ayelenmachicado2001@gmail.com>
2024-03-20 03:57:20 -0300

    Re entrega Desafio 2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class ProductManager {
     constructor(archivo) {
         this.path = archivo;
         this.products = [];
-        this.getProducts();
+        // this.getProducts();
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
@@ -39,9 +39,9 @@ class ProductManager {
     // Leer productos
     getProducts = async () => {
         let respuesta = await fs.promises.readFile(this.path, "utf-8");
-        this.products = JSON.parse(respuesta);
-        console.log(this.products);
-        return this.products;
+        let nuevoArray = JSON.parse(respuesta);
+        console.log(nuevoArray);
+        return nuevoArray;
     }
 
     getProductById(id) {
@@ -55,20 +55,41 @@ class ProductManager {
             return product;
         }
     }
-    // Borrar productos
+    // Borrar productos REVISAR
     deleteProducts = async (id) => {
         this.products = this.products.filter(item => item.id !== id);
         await this.guardarArchivos();
         console.log("Producto eliminado correctamente");
     }
+    // Actualizar productos
+    // El método recibe tres parametros, el id del objeto, la propiedad que vamos a actualizar y su valor nuevo
+    updateProduct = async (id, propiedad, nuevoValor) => {
+        const idBuscado = this.products.findIndex(item => item.id === id);
+        if (idBuscado !== -1) {
+            this.products[idBuscado][propiedad] = nuevoValor;
+            await this.guardarArchivos();
+            console.log("Se actualizó el producto! Esta es la lista nueva:");
+            await this.getProducts();
+        } else {
+            console.log("El ID proporcionado no existe en la lista de productos");
+        }
+    }
 }
 
 const ruta = new ProductManager("./archivo-productos.json");
 
+// Testing
+// 
 ruta.addProduct("Campera", "Campera de invierno impermiable con capucha disponible en los colores rojo, verde y azul.", 500, "Sin imagen", "C001", 10);
 ruta.addProduct("Camisa", "Camisa a cuadros unisex disponible en los colores rojo, verde y gris.", 1500, "Sin imagen", "C002", 5);
 ruta.addProduct("Zapatillas", "Zapatillas marca Jaguar unisex hasta el tobillo desde talle 30 hasta 37, consultar previamente por colores disponibles.", 2000, "Sin imagen", "C003", 20);
 ruta.addProduct("Medias", "Medias de abejita amarillas con rayas negras disponible en los talles 20 hasta 37", 500, "Sin imagen", "C004", 40);
 
-ruta.deleteProducts(3);
-console.log(ruta.getProducts());
+// 
+ruta.getProducts();
+// 
+ruta.getProductById(10);
+// 
+ruta.updateProduct(1, "price", 20999990);
+// 
+ruta.deleteProducts(20);
