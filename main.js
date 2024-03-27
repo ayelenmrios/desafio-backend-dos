082a9c3e7ff44261b3b62fc82491f1c2129413ab
Ayelen Machicado <ayelenmachicado2001@gmail.com>
2024-03-27 02:51:06 -0300

    Correciones Desafio 2

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 
+// PRODUCT MANAGER
 class ProductManager {
     static lastId = 0;
+
     constructor(archivo) {
-        this.path = archivo;
         this.products = [];
-        // this.getProducts();
+        this.path = archivo;
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
@@ -17,6 +18,7 @@ class ProductManager {
             console.log("Se repite el código de los productos. Por favor revise los valores.");
             return;
         }
+
         const newProduct = {
             id: ++ProductManager.lastId,
             title,
@@ -25,25 +27,31 @@ class ProductManager {
             thumbnail,
             code,
             stock
-        };
+        }
+
         this.products.push(newProduct);
         this.guardarArchivos();
         // Cada vez que se agregue un producto al array se guarda automáticamente.
     }
-
-    // Guardar archivo con productos
-    guardarArchivos = async () => {
-        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+    /////////////////////////////////
+    // MÉTODOS
+    // getProducts
+    async getProducts(){
+        // Llamo al metodo leerProducts();
+        const lectura = await this.leerProductos();
+        return lectura;
     }
+    async leerProductos() {
+        try {
+            const lectura = await fs.promises.readFile(this.path, "utf-8");
+            const respuesta = JSON.parse(lectura);
+            return respuesta;
 
-    // Leer productos
-    getProducts = async () => {
-        let respuesta = await fs.promises.readFile(this.path, "utf-8");
-        let nuevoArray = JSON.parse(respuesta);
-        console.log(nuevoArray);
-        return nuevoArray;
+        } catch (error) {
+            console.log("Se guardaron mal los productos, no se pueden leer", error);
+        }
     }
-
+    // Método getProductById
     getProductById(id) {
         const product = this.products.find(item => item.id === id);
 
@@ -52,44 +60,59 @@ class ProductManager {
             return null;
         } else {
             console.log("Producto encontrado!");
+            console.log(product);
             return product;
         }
     }
-    // Borrar productos REVISAR
-    deleteProducts = async (id) => {
-        this.products = this.products.filter(item => item.id !== id);
-        await this.guardarArchivos();
-        console.log("Producto eliminado correctamente");
+    // Guardar archivo con productos
+    async guardarArchivos() {
+       await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
     }
-    // Actualizar productos
-    // El método recibe tres parametros, el id del objeto, la propiedad que vamos a actualizar y su valor nuevo
-    updateProduct = async (id, propiedad, nuevoValor) => {
+
+    // Actualizar el producto
+    async updateProduct(id, propiedad, nuevoValor) {
         const idBuscado = this.products.findIndex(item => item.id === id);
         if (idBuscado !== -1) {
-            this.products[idBuscado][propiedad] = nuevoValor;
+            this.products[idBuscado][propiedad] = Number(nuevoValor);
             await this.guardarArchivos();
-            console.log("Se actualizó el producto! Esta es la lista nueva:");
-            await this.getProducts();
         } else {
-            console.log("El ID proporcionado no existe en la lista de productos");
+            console.log("Ese id no existe");
         }
     }
+    // Borrar producto del array
+    async deleteProduct(id) {
+        try {
+            let productos = await this.leerProductos(); 
+            let indice = productos.findIndex(producto => producto.id === id); 
+    
+            if (indice !== -1) {
+                productos.splice(indice, 1); 
+                this.products = productos; 
+                await this.guardarArchivos();
+                
+            } else {
+                console.log("El producto que quiere eliminar no existe");
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    //////////////////////////
 }
+//////////////////////////////////////
+const ruta = new ProductManager("./archivoProductos.json");
 
-const ruta = new ProductManager("./archivo-productos.json");
 
+///////////////////////////////////
 // Testing
-// 
-ruta.addProduct("Campera", "Campera de invierno impermiable con capucha disponible en los colores rojo, verde y azul.", 500, "Sin imagen", "C001", 10);
-ruta.addProduct("Camisa", "Camisa a cuadros unisex disponible en los colores rojo, verde y gris.", 1500, "Sin imagen", "C002", 5);
+ruta.addProduct("Campera", "Campera de invierno impermiable con capucha disponible en los colores rojo, verde y azul.", 5, "Sin imagen", "C001", 10);
+ruta.addProduct("Camisa", "Camisa a cuadros unisex disponible en los colores rojo, verde y gris.", 2, "Sin imagen", "C002", 5);
 ruta.addProduct("Zapatillas", "Zapatillas marca Jaguar unisex hasta el tobillo desde talle 30 hasta 37, consultar previamente por colores disponibles.", 2000, "Sin imagen", "C003", 20);
 ruta.addProduct("Medias", "Medias de abejita amarillas con rayas negras disponible en los talles 20 hasta 37", 500, "Sin imagen", "C004", 40);
 
-// 
 ruta.getProducts();
-// 
-ruta.getProductById(10);
-// 
-ruta.updateProduct(1, "price", 20999990);
-// 
-ruta.deleteProducts(20);
+ruta.getProductById(1);
+ruta.deleteProduct(4);
+ruta.updateProduct(1, "price", 6);
